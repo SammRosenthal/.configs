04357c9ea34bf65cb2904dfd4dc3f7e8bbee28ab
Sam Rosenthal <39362114+SammRosenthal@users.noreply.github.com>
2025-06-11 08:55:53 -0700

    Add files via upload

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,21 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-  opts_extend = { "ensure_installed" },
-  opts = {
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = true },
-    auto_install = true,
-    ensure_installed = {
-      "c", "lua", "rust", "help", "javascript", "typescript", "svelte", "java", "astro", "css", "tsx" },
-  },
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+      opts_extend = { "ensure_installed" },
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+      auto_install = true,
+      ensure_installed = {
+        "c", "lua", "rust", "javascript", "typescript", "svelte", "java", "astro", "css", "tsx"
+      },
+    })
+  end
 }
